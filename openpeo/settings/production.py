ded34dbacb4d699efb6e2f10550e3b182a328f9d
PUYUP <hellopuyup@gmail.com>
2020-09-14 12:00:01 +0700

    Update

--- openpeo/settings/production.py
+++ openpeo/settings/production.py
@@ -47,14 +47,14 @@ X_FRAME_OPTIONS = 'DENY'
 # Django csrf
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/2.2/ref/csrf/
+CSRF_COOKIE_DOMAIN = 'openpeo.com'
 CSRF_HEADER_NAME = 'HTTP_X_CSRFTOKEN'
 CSRF_COOKIE_SECURE = True
 CSRF_USE_SESSIONS = False
 CSRF_COOKIE_HTTPONLY = False
 CSRF_TRUSTED_ORIGINS = [
     'openpeo-dev.firebaseapp.com',
-    'openpeo.com',
-    'api.openpeo.com'
+    'openpeo.com'
 ]
 
 
